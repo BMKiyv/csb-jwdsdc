568931fc320502adfa303c50e224fcf8952e5e5c
Code Sandbox <hello@codesandbox.io>
2023-09-25 09:04:35 +0000

    Deployment from Codesandbox.

---
+++ index.js
@@ -0,0 +1,93 @@
+//console.log('it is working from localhost!!!!!!!')
+//let animated = document.querySelector(".slider-wrapper");
+let button = document.querySelectorAll(".div-block-7");
+let header = document.querySelector("header");
+let body = header.nextElementSibling;
+let footer = document.querySelector("footer");
+let customLocation = document.location;
+let about = document.querySelector(".about");
+let activity = document.querySelector(".activity");
+let menu1 = document.querySelector(".second-menu");
+let menu2 = document.querySelector(".menu-block");
+let counter = localStorage.getItem("filterCounter")
+  ? localStorage.getItem("filterCounter")
+  : 0;
+let uaLang = document.querySelectorAll(".lang-ua");
+let enLang = document.querySelectorAll(".lang-en");
+let languages = document.querySelectorAll(".div-block-8");
+// console.log(customLocation.pathname.slice(0,3), customLocation.pathname)
+
+if (customLocation.pathname.slice(0, 3) !== "/en") {
+  for (let item of uaLang) {
+    item.classList.add("active");
+  }
+  for (let item of enLang) {
+    item.classList.remove("active");
+  }
+} else {
+  for (let item of uaLang) {
+    item.classList.remove("active");
+  }
+  for (let item of enLang) {
+    item.classList.add("active");
+  }
+}
+
+for (let item of languages) {
+  item.addEventListener("click", (e) => {
+    let target = e.target;
+
+    if (target === uaLang[0] || target === uaLang[1]) {
+      let url = `${customLocation.origin}/alt-main`;
+      customLocation.assign(url);
+    } else {
+      let url = `${customLocation.origin}/en/alt-main`;
+      customLocation.assign(url);
+    }
+  });
+}
+if (
+  localStorage.getItem("filtered") === "1" &&
+  localStorage.getItem("filterCounter") % 2
+) {
+  header.classList.add("filter");
+  body.classList.add("filter");
+  footer.classList.add("filter");
+}
+for (let item of button) {
+  item.addEventListener("click", () => {
+    counter++;
+    if (counter % 2) {
+      header.classList.add("filter");
+      body.classList.add("filter");
+      footer.classList.add("filter");
+      localStorage.setItem("filtered", "1");
+      localStorage.setItem("filterCounter", counter);
+    } else {
+      header.classList.remove("filter");
+      body.classList.remove("filter");
+      footer.classList.remove("filter");
+      localStorage.setItem("filtered", "0");
+      localStorage.setItem("filterCounter", counter);
+    }
+    //console.log(counter);
+  });
+}
+about.addEventListener("click", (e) => {
+  menu2.style.height = "0px";
+  console.log(e.target);
+  if (!menu1.style.height || menu1.style.height === "0px") {
+    menu1.style.height = "64px";
+  } else {
+    menu1.style.height = "0px";
+  }
+});
+activity.addEventListener("click", (e) => {
+  menu1.style.height = "0px";
+  console.log(e.target);
+  if (!menu2.style.height || menu2.style.height === "0px") {
+    menu2.style.height = "128px";
+  } else {
+    menu2.style.height = "0px";
+  }
+});
